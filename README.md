bcb50840acd51e71ef1732025e506f2a46d5ebba
chaiwith <a.chaiwith@gmail.com>
2017-05-05 00:38:42 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # workshop-netpie-android
+
+
+## Microgear-android
+-----------
+microgear-android คือ client library สำหรับ Android Studio ที่ทำหน้าที่เป็นตัวกลางในการเชื่อมโยง application code เข้ากับบริการของ netpie platform เพื่อการพัฒนา IOT application รายละเอียดเกี่ยวกับ netpie platform สามารถศึกษาได้จาก http://netpie.io
+
+## การติดตั้ง
+-----------
+สามารถเรียกใช้เวอร์ชั่นล่าสุดจาก Jcenter โดยใช้ Gradle 
+```java
+compile 'io.netpie:microgear:1.1.3'
+```
---
+++ lab 1 android monitor/MainActivity.java
@@ -0,0 +1,112 @@
+package com.myname.myapplication;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.netpie.microgear.Microgear;
+import io.netpie.microgear.MicrogearEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private Microgear microgear = new Microgear(this);
+    private String appid = "appid"; //APP_ID
+    private String key = "key"; //KEY
+    private String secret = "secret"; //SECRET
+    private String alias = "android";
+    private TextView myTextView;
+    private Button button_on;
+    private Button button_off;
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String string = bundle.getString("state");
+            myTextView = (TextView) findViewById(R.id.state);
+            myTextView.setText(" " + string);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MicrogearCallBack callback = new MicrogearCallBack();
+        microgear.resettoken();
+        microgear.setCallback(callback);
+        microgear.connect(appid, key, secret, alias);
+        microgear.subscribe("/pieled/state");
+        button_on = (Button) findViewById(R.id.button_on);
+        button_on.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                microgear.chat("pieled", "1");
+            }
+        });
+        button_off = (Button) findViewById(R.id.button_off);
+        button_off.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                microgear.chat("pieled", "0");
+            }
+        });
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        microgear.disconnect();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        microgear.bindServiceResume();
+    }
+
+    class MicrogearCallBack implements MicrogearEventListener {
+        @Override
+        public void onConnect() {
+            Log.i("Connected", "Now I'm connected with netpie");
+        }
+
+        @Override
+        public void onMessage(String topic, String message) {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            if (message.equals("1")) {
+                bundle.putString("state", "ON");
+            } else {
+                bundle.putString("state", "OFF");
+            }
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("Message", topic + " : " + message);
+        }
+
+        @Override
+        public void onPresent(String token) {
+            Log.i("present", "New friend Connect :" + token);
+        }
+
+        @Override
+        public void onAbsent(String token) {
+            Log.i("absent", "Friend lost :" + token);
+        }
+
+        @Override
+        public void onDisconnect() {
+            Log.i("disconnect", "Disconnected");
+        }
+
+        @Override
+        public void onError(String error) {
+            Log.i("error", "Error : " + error);
+        }
+
+        @Override
+        public void onInfo(String info) {
+            Log.i("info", "Info : " + info);
+        }
+    }
+}
